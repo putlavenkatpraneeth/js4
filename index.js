23275d0a583883f0ead10c5b2623c8745a9fb9ef
putla venkat praneeth <putlavenkat123@gmail.com>
2024-10-23 19:49:42 +0530

    changes

---
+++ index.js
@@ -0,0 +1,21 @@
+var arr=[1,2,3];
+arr.push(4);
+console.log(arr);//[ 1, 2, 3, 4 ]
+
+var arr=[1,2,3,4];
+arr.pop();
+console.log(arr);//[ 1, 2, 3 ]
+
+var arr=[2,3,4];
+arr.unshift(1);
+console.log(arr);//[ 1, 2, 3, 4 ]
+
+var arr=[1,2,3,4];
+arr.shift();
+console.log(arr);//[ 2, 3, 4 ]
+
+var arr=[1,2,3];
+console.log(arr.join(""));//123
+
+
+
